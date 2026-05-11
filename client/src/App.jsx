ccc51f58754bf076b9661729d815eb4aa6b8ad05
Nsengiyunva <kingbecks07@gmail.com>
2026-05-11 16:37:40 +0300

    updates the kube

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          This is the Front end React App
+        </div>
+       
+      </section>
+    </>
+  )
+}
+
+export default App
